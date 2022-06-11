ea3f1c3bd77ad5019e7f33f6f82d20caf50d30e0
Danya Karras <Danya.Karras@D2L.com>
2022-06-11 11:20:16 -0700

    decrement_counter

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,16 @@
 import {useSelector, useDispatch} from 'react-redux';
-import { increment } from '../actions/index.js'
+import { increment, decrement } from '../actions/index.js'
 
 export default function Button() {
     const count = useSelector(state => state.buttonCount);
 
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
 
   return (
     <div>
       <h1>I want {count} scoops of ice cream!</h1>
-      <button onClick={() => dispatch(increment(1))}>More!</button>
+      <button onClick={() => dispatch(increment(5))}>More!</button>
+      <button onClick={() => dispatch(decrement(5))}>Less!</button>
     </div>
   );
 }
